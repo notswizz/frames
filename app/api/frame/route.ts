e655a60e3dc1bb3be691e364384dd7a8869ebcc3
swizz <emailswizz@gmail.com>
2024-02-10 14:51:18 -0500

    ok

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,3 +1,5 @@
+// api/frame/route.tsx
+
 import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
@@ -9,26 +11,70 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
+  // Validate the frame message
   if (isValid) {
     accountAddress = message.interactor.verified_accounts[0];
   }
 
+  // Extract text input from the frame message
   if (message?.input) {
     text = message.input;
   }
 
+  // Handle redirect based on button clicked and conditions like 'liked'
   if (message?.button === 3) {
-    return NextResponse.redirect(
-      'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
-      { status: 302 },
-    );
+    // If 'Redirect to pictures' button is clicked and user has liked the post
+    if (message?.liked) {
+      return NextResponse.redirect(
+        'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
+        { status: 302 },
+      );
+    } else {
+      // User has clicked the button but hasn't liked the post, prompt them to like
+      return new NextResponse(
+        getFrameHtmlResponse({
+          buttons: [
+            {
+              label: 'Story time!',
+            },
+            {
+              action: 'link',
+              label: 'Link to Google',
+              target: 'https://www.google.com',
+            },
+            {
+              label: 'Like to unlock pictures',
+              action: 'post_redirect',
+            },
+          ],
+          image: {
+            src: `${NEXT_PUBLIC_URL}/park-3.png`,
+            aspectRatio: '1:1',
+          },
+          input: {
+            text: 'Tell me a boat story',
+          },
+          postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+        }),
+      );
+    }
   }
 
+  // Return a new frame response with the preserved text input in the button label
   return new NextResponse(
     getFrameHtmlResponse({
       buttons: [
         {
-          label: `Story: ${text} 🌲`,
+          label: `🌲☀️ ${text} 🌲🌲`,
+        },
+        {
+          action: 'link',
+          label: 'Link to Google',
+          target: 'https://www.google.com',
+        },
+        {
+          label: 'Redirect to pictures',
+          action: 'post_redirect',
         },
       ],
       image: {
